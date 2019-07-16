6e6131cb7f9fc2ebaf47206a9b7fcc5f18fd0636
surin-0 <marblegiraffe@ajou.ac.kr>
2019-07-16 20:56:17 +0900

    modify add and subtract method in CustomCalculatorTest class

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -10,20 +10,26 @@ public class CustomCalculatorTest {
     private CustomCalculator customCalculator;
 
     //더하기 테스트 작성
+    //박수린
     @Test
     public void add() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.add(10,15);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(25));
+        assertFalse(result == 10);
         System.out.println("result :: " + result);
     }
 
     //빼기 테스트 작성
+    //박수린
     @Test
     public void subtract() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertFalse(result == 12);
+        assertTrue(result == 13);
         System.out.println("result :: " + result);
     }
 
