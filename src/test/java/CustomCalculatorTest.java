b6a49311d8e69b48c8b88c7270966b9d6df1c16e
Joylish <yeonzzu45@ajou.ac.kr>
2019-07-21 17:20:00 +0900

    add test code to test method of multiply and divide using assertThat

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -52,22 +52,24 @@ public class CustomCalculatorTest {
         assertTrue(result == 13);
         System.out.println("result :: " + result);
     }
-    
+
+    //이연주
     //곱하기 테스트 작성
     @Test
     public void multiply() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.multiply(5,9);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(45));
         System.out.println("result :: " + result);
     }
 
+    //이연주
     //나누기 테스트 작성
     @Test
     public void divide() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(5));
         System.out.println("result :: " + result);
     }
 }
